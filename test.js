a5d5ae081835c29a94095f3fde2c15f038dd01dc
xuechaogit <793866547@qq.com>
2023-06-09 20:20:27 +0800

    simple oparetor

---
+++ test.js
@@ -0,0 +1,16 @@
+//git add
+//git init
+//git commit -m 'description'
+
+//git log
+//git log --pretty=oneline
+
+//git rest --hard HEAD^^^
+//git rest --hear 版本号前几位
+
+//rm 文件名
+//git rm 文件名
+//git commit -m 'description'
+
+//rm
+//git checkout -- 文件名
